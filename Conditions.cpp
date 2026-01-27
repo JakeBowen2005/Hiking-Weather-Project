1aa960a8ec395a9aed9352636f4af293fcd200ea
Jake Bowen <jakeeb05@gmail.com>
2026-01-26 18:31:09 -0800

    Adding the start time feature and creatign the start time object

--- Conditions.cpp
+++ Conditions.cpp
@@ -6,6 +6,7 @@ void Conditions::print() const {
     std::cout << "Time: " << time << "\n";
     std::cout << "Weather code: " << weather_code << "\n";
     std::cout << "Temp: " << temp_f << " F (feels like " << apparent_temp_f << " F)\n";
-    std::cout << "Wind: " << wind_speed_mph << "\n";
+    std::cout << "Wind Speed: " << wind_speed_mph << "\n";
+    std::cout << "Wind gusts: " << wind_gusts_mph << "\n";
     std::cout << "Precip: " << precipitation_in << " in\n";
 }
--- main.cpp
+++ main.cpp
@@ -7,7 +7,6 @@
 //compile command
 // g++ -std=c++17 -Wall -Wextra -pedantic -O2 -o app main.cpp WeatherClient.cpp -lcurl
 
-using namespace std;
 
 int main(int argc, char* argv[])
 {
@@ -16,11 +15,13 @@ int main(int argc, char* argv[])
     std::string location = "Mt Baldy";
     int start_time = 6;
     int duration = 1;
+    int increment = 1;
 
 
     app.add_option("--l, --location", location, "Location");
     app.add_option("--s, --start_time", start_time, "Start hour (0-23) for today")->check(CLI::Range(0,23));
-    app.add_option("--d, --duration", duration, "Duration of Hike (hours)")->check(CLI::PositiveNumber);
+    app.add_option("--i, --increment", increment, "Increment of hours for each forecast")->check(CLI::PositiveNumber);
+    app.add_option("--d, --duration", duration, "Duration of the hike in hours")->check(CLI::PositiveNumber);
     CLI11_PARSE(app, argc, argv);
 
     // cout << "Location: " << location << endl;
@@ -29,7 +30,7 @@ int main(int argc, char* argv[])
 
     //making location url safe with lambda function
     auto encode_for_query = [](const std::string& s) {
-        string out;
+        std::string out;
         out.reserve(s.size());
         for ( unsigned char c : s) {
             if (c == ' ') {
@@ -43,40 +44,39 @@ int main(int argc, char* argv[])
 
     //function to get correct start time
     auto pad2 = [](int h) {
-        string s = to_string(h);
+        std::string s = std::to_string(h);
         if (s.size() == 1) s= "0"+s;
         return s;
     };
 
+
     //make url to get lat and long
-    const string encoded_location = encode_for_query(location);
-    const string geo_url = "https://geocoding-api.open-meteo.com/v1/search?name="
+    const std::string encoded_location = encode_for_query(location);
+    const std::string geo_url = "https://geocoding-api.open-meteo.com/v1/search?name="
                             + encoded_location + "&count=10&language=en&format=json";
 
 
 
     WeatherClient client;
 
-    string geo_body = client.get(geo_url); //raw json text
+    std::string geo_body = client.get(geo_url); //raw json text
     using json = nlohmann::json;
     json j = json::parse(geo_body);
 
     if (!j.contains("results") || j["results"].empty()) {
-        cerr << "No weather results for: " << location << "\n";
+        std::cerr << "No weather results for: " << location << "\n";
         return 1;
     }
     const json& first = j["results"][0];
     double lat = first.at("latitude").get<double>();
     double lon = first.at("longitude").get<double>();
 
-    cout << "Location: " << location << "\n";
-    cout << "Lat: " << lat << "\n";
-    cout << "Lon: " << lon << "\n";
+    std::cout << "Location: " << location << "\n";
+    std::cout << "Lat: " << lat << "\n";
+    std::cout << "Lon: " << lon << "\n";
 
-    //Current Conditions
-    Conditions Curr;
     //forecast api call
-    const string forecast_url =
+    const std::string forecast_url =
         "https://api.open-meteo.com/v1/forecast?latitude=" + std::to_string(lat) +
         "&longitude=" + std::to_string(lon) +
         "&current=temperature_2m,apparent_temperature,precipitation,weather_code,wind_speed_10m,wind_gusts_10m,wind_direction_10m"
@@ -90,17 +90,19 @@ int main(int argc, char* argv[])
     // std::cout << "\n=== Forecast API URL ===\n";
     // std::cout << forecast_url << "\n\n";
 
-    const string forecast_body = client.get(forecast_url); //raw json text
+    const std::string forecast_body = client.get(forecast_url); //raw json text
     // std::cout << "=== Raw Forecast API Response ===\n";
     // std::cout << forecast_body << "\n";
 
     json j_forecast = json::parse(forecast_body);
 
     if (!j_forecast.contains("current") || j_forecast["current"].empty()) {
-        cerr << "No Weather results for: " << location << "\n";
+        std::cerr << "No Weather results for: " << location << "\n";
         return 1;
     }
 
+    // Current Conditions
+    Conditions Curr;
     const json& current_conditions = j_forecast["current"];
     Curr.weather_code = current_conditions.at("weather_code").get<int>();
     Curr.temp_f = current_conditions.at("temperature_2m").get<double>();
@@ -108,14 +110,42 @@ int main(int argc, char* argv[])
     Curr.wind_speed_mph = current_conditions.at("wind_speed_10m").get<double>();
     Curr.precipitation_in = current_conditions.at("precipitation").get<double>();
     Curr.wind_gusts_mph = current_conditions.at("wind_gusts_10m").get<double>();
-    Curr.time = current_conditions.at("time").get<string>();
+    Curr.time = current_conditions.at("time").get<std::string>().substr(12,16);
+    Curr.print();
 
-    // cout << "Weather Code: " << c_weather_code << "\n";
-    // cout << "Current temp: " << c_temp << "\n";
-    // cout << "Apparent temp: " << c_apparent_temp << "\n";
-    // cout << "Wind Speed: " << c_wind_speed << "\n";
-    // cout << "Precipation: " << c_precipitation << "\n";
-    // cout << "Current time: " << curr_time << "\n";
+    //Build timestamp for start time
+    const std::string date_part = Curr.time.substr(0,10);
+    const std::string start_target_time = date_part + "T" + pad2(start_time) + ":00";
+
+    if (!j_forecast.contains("hourly") || !j_forecast["hourly"].contains("time")) {
+        std::cerr << "Forecast response missing hourly.time; cannot look up start hour \n";
+        return 1;
+    }
+
+    const json& hourly = j_forecast["hourly"];
+    const auto& times = hourly.at("time");
+
+    int start_idx = -1;
+    for (std::size_t i = 0; i < times.size(); i++) {
+        if (times[i].get<std::string>() == start_target_time) {
+            start_idx = static_cast<int>(i);
+            break;
+        }
+    }
+    
+    if (start_idx == -1) {
+        std::cerr << "Could not find start time in hourly forecast";
+    } 
+
+    //Start time conditons
+    Conditions Start;
+    Start.label = "Starting Time of the hike";
+    Start.time = start_target_time.substr(12,16);
+    Start.weather_code = hourly.at("weather_code")[start_idx].get<int>();
+
+
+
+
 
 
 }
